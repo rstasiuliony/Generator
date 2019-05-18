6eca787cb2e81e8e9d587d55dabc51a63362c7c4
Renata <renata.stasiulionyte@gmail.com>
2019-05-18 23:09:16 +0300

    Created second test testing with big coefficient

--- src/test/java/testGenerator/GeneratorTest.java
+++ src/test/java/testGenerator/GeneratorTest.java
@@ -12,23 +12,41 @@ import static org.junit.Assert.assertEquals;
 public class GeneratorTest {
 
     private Generator generatorSmallCoefficient;
+    private Generator generatorBigCoefficient;
+    private List<Long> correctValues;
+    private List<Long> generatedList;
 
     @Before
     public void setUp() throws Exception {
 
         generatorSmallCoefficient = new Generator(65, Coefficient.SMALL.getCoefficient(), 4);
+        generatorBigCoefficient = new Generator(8921, Coefficient.BIG.getCoefficient(), 4);
     }
 
     @Test
-    public void testGetListOfGeneratedValues() {
+    public void testGetListOfGeneratedValuesWithSmallCoefficient() {
 
-        List<Long> correctValues = new ArrayList<>();
+        correctValues = new ArrayList<>();
         correctValues.add(1092455L);
         correctValues.add(1181022009L);
         correctValues.add(245556042L);
         correctValues.add(1744312007L);
 
-        List<Long> generatedList = generatorSmallCoefficient.getListOfGeneratedValues();
+        generatedList = generatorSmallCoefficient.getListOfGeneratedValues();
+
+        assertEquals(correctValues, generatedList);
+    }
+
+    @Test
+    public void testGetListOfGeneratedValuesWithBigCoefficient() {
+
+        correctValues = new ArrayList<>();
+        correctValues.add(430625591L);
+        correctValues.add(1233683848L);
+        correctValues.add(1431495498L);
+        correctValues.add(137874439L);
+
+        generatedList = generatorBigCoefficient.getListOfGeneratedValues();
 
         assertEquals(correctValues, generatedList);
     }
